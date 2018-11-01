56929e98ace462a5e3a6683bbccc6b7c4b96e3e5
5donuts <charliegerman32@gmail.com>
2018-11-01 14:22:29 -0400

    reformatted 2 long lines

--- scale-and-crop-images.py
+++ scale-and-crop-images.py
@@ -13,11 +13,13 @@ def scale_and_crop(img):
     aspect_ratio = width / height
     if width < height:
         scaled_img = img.resize((SIZE, int(SIZE / aspect_ratio)), Image.ANTIALIAS)
-        cropped_img = scaled_img.crop((0, int((SIZE/aspect_ratio - SIZE) / 2), SIZE, SIZE + int((SIZE/aspect_ratio - SIZE) / 2)))
+        cropped_img = scaled_img.crop((0, int((SIZE / aspect_ratio - SIZE) / 2),
+            SIZE, SIZE + int((SIZE / aspect_ratio - SIZE) / 2)))
         return cropped_img
     else:
         scaled_img = img.resize((int(SIZE * aspect_ratio), SIZE), Image.ANTIALIAS)
-        cropped_img = scaled_img.crop((int((SIZE*aspect_ratio - SIZE) / 2), 0, SIZE + int((SIZE*aspect_ratio - SIZE) / 2), SIZE))
+        cropped_img = scaled_img.crop((int((SIZE * aspect_ratio - SIZE) / 2), 0,
+            SIZE + int((SIZE * aspect_ratio - SIZE) / 2), SIZE))
         return cropped_img
 
 # get the second-to-last occurrence of pattern in text
