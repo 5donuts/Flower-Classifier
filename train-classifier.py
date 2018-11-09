5f267f263db1bdf18da86e405a02bf34efc7f7d1
Istrali <istrali775@gmail.com>
2018-11-09 12:25:34 -0500

    confusion matrix

--- train-classifier.py
+++ train-classifier.py
@@ -169,6 +169,21 @@ plt.plot(epochs, val_loss)
 plt.title('Training and validation loss')
 """
 
+"""
+#Print confusion matrix of all items in validation set
+evaluation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(240, 240),
+    batch_size=1,
+    class_mode='categorical',
+    shuffle=False)
+
+y_pred_frac = model.predict_generator(evaluation_generator, evaluation_generator.classes.size)
+y_pred = np.argmax(y_pred_frac, axis=1)
+print('Confusion Matrix')
+print(confusion_matrix(evaluation_generator.classes, y_pred))
+"""
+
 # save the weights to a file
 output_dir = 'weights'
 output_file = os.path.join(output_dir, 'flower-weights')
